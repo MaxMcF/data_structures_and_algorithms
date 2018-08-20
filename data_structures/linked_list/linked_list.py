7be2077f04e2b0c6f048556a52d3868a0d1b87c1
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-20 15:34:36 -0700

    added ll_insertions

--- data_structures/linked_list/linked_list.py
+++ data_structures/linked_list/linked_list.py
@@ -20,17 +20,60 @@ class LinkedList(object):
         return self._length
 
     def insert(self, val: Any) -> Any:
-        prev_node = self.head
-        self.head = Node(val, prev_node)
+        '''Creates a new node and appends it to the beginning of the list
+            This is done by re-stating which node is the head(the one youre appending)
+            then setting the previous head equal to the next value of the appended item
+        '''
+        self.head = Node(val, self.head)
         self._length += 1
 
     def includes(self, val) -> bool:
+        '''Checks to see if a value is contained in the list by iterating through and
+        checking every node against the input value. If the value exists in the list
+        , the function will return true if it does not exist in the list, it will return false.
+        '''
         current = self.head
         while current is not None:
             if current.val is val:
                 return True
             current = current.next
         return False
+
+    def append(self, val) -> bool:
+        '''Creates a new node and appends it at the end of the linked list
+        '''
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = Node(val, None)
+        return True
+
+    def insert_before(self, val, target_val) -> bool:
+        '''Creates a new node and appends it before the target value
+            args:
+                new Node value, target Node value to insert before
+            returns:
+                True if the node was inserted
+                False if the target value is not in the SLL
+        '''
+        current = self.head
+        while current.next.val is not target_val:
+            current = current.next
+            if current.next == None:
+                return False
+        current.next = Node(val, current.next)
+        return True
+
+    def insert_after(self, val, target_val) -> bool:
+        current = self.head
+        while current.val is not target_val:
+            current = current.next
+            if current == None:
+                return False
+        current.next = Node(val, current.next)
+        return True
+
+
 
 
 # def small_list():
