3f4d6c1ba1b357b126984f73e11425fb7a358fca
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-28 20:32:29 -0700

    added docstrings

--- challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,66 +1,50 @@
 class Node:
-    def __init__(self, value, _next):
-        self.val = value
+    """This creates an animal instance
+    """
+    def __init__(self, _next, data, spec):
         self.next = _next
+        self.tag = data
+        self.cat_or_dog = spec
 
     def __repr__(self):
-        return f'< Value: {self.val} | Next: {self.next.val} >'
+        return f'< Species: {self.spec} | Next: {self.next.spec} | Tag: {self.tag} >'
 
-class Stack:
-    def __init__(self, vals=None):
-        self.length = 0
-        self.top = None
-        if vals is not None:
-            for i in vals:
-                self.push(i)
-
-    def __len__(self):
-        return self.length
-
-    def __str__(self):
-        return f'Stack Top: {self.top} | Stack Length: {len(self)}'
-
-    def push(self, value):
-        self.top = Node(value, self.top)
-        self.length += 1
-        return self.top.val
-
-    def pop(self):
-        temp = self.top
-        self.top = self.top.next
-        return temp.val
-
-    def peek(self):
-        return self.top
 
 class AnimalShelter:
+    """This is the Animal shelter linked list class, which allows a user to add
+    animals by using the enqueue method, and adopt animals using the dequeue method.
+    """
     def __init__(self):
-        self.length = 0
-        self.front = None
-        self.back = None
+        self.tags = 0
 
     def __len__(self):
-        return self.length
+        return self.tags
 
-    def enqueue(self, value):
-        self.back = Node(value, self.back)
-        self.length += 1
-        if self.length == 1:
+    def enqueue(self, spec):
+        self.tags += 1
+        self.back = Node(self.back, self.tags, spec)
+        if self.tags == 1:
             self.front = self.back
         return self.back.val
 
     def dequeue(self, prefrence):
-        stack = Stack()
+        adopted = None
         while self.back is not None:
-            temp = self.back
-            stack.push(temp)
-            self.back = self.back.next
+            if self.back.val == prefrence:
+                adopted = self.back
+                self.back = self.back.next
+        return adopted
         print(stack.peek)
-        while stack.peek.val is not prefrence:
+        while stack. is not prefrence:
             self.enqueue(stack.pop())
         adopted = stack.pop()
         while stack.peek is not None:
             self.enqueue(stack.pop())
         return adopted
 
+# class dogs(AnimalShelter):
+#     """
+#     """
+#     def __init__(self):
+#         self.
 
