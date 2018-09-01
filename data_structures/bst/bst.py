bcf1451b182e7edc44a542ad7dbe5327b384b6bd
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-31 17:18:28 -0700

    adding tests

--- data_structures/bst/bst.py
+++ data_structures/bst/bst.py
@@ -8,9 +8,14 @@ class Node:
     def __str__(self):
         return f'Value: {self.value} | Data: {self.data}'
     def __repr__(self):
-        pass
+        return f'< Value: {self.value} | Data: {self.data} | Left: {self.left} | Right: {self.right} >'
 
 class BinaryTree:
+    """This is a class method that creates a binary tree. The tree can be initialized without
+    any input, or with an iterable.
+    Running the insert method will filter down the tree recursively to find the spot that the
+    node belongs (corresponding to value).
+    """
     def __init__(self, iterable=None):
         self.root = None
         if iterable is not None:
@@ -18,10 +23,10 @@ class BinaryTree:
                 self.insert(i)
 
     def __str__(self):
-        return f'Root: {self.root} | '
+        return f'Root: {self.root} | Tree in order: {self.in_order}'
 
     def __repr__(self):
-        pass
+        return f'<In_order: {self.in_order} | Pre_order: {self.pre_order} | Post_order: {self.post_order}'
 
     def insert(self, value):
         if self.root is None:
@@ -31,20 +36,26 @@ class BinaryTree:
             if current_node.value > value:
                 if current_node.left is None:
                     current_node.left = Node(value)
-                else:
-                    _walk(current_node.left, value)
-            else:
+                    return True
+                _walk(current_node.left, value)
+            elif current_node.value < value:
                 if current_node.right is None:
                     current_node.right = Node(value)
-                else:
-                    _walk(current_node.right, value)
+                    return True
+                _walk(current_node.right, value)
+            else:
+                return False
 
-        _walk(self.root, value)
+        bool_success = _walk(self.root, value)
+        print(bool_success)
+        return bool_success
+
 
 
     def in_order(self, callable=lambda node: print(node.value)):
         """Go left: visit, visit, go right: visit
         """
+        tree_list = []
         def _walk(node=None):
             if node is None:
                 return
@@ -54,24 +65,27 @@ class BinaryTree:
                 _walk(node.left)
 
             # visit
-            callable(node)
+            tree_list.append(node.value)
+            # callable(node)
 
             # Go right
             if node.right is not None:
                 _walk(node.right)
 
         _walk(self.root)
+        return tree_list
 
     def pre_order(self, callable=lambda node: print(node.value)):
         """visit, go left go right
         """
+        tree_list = []
         def _walk(node=None):
             if node is None:
                 return
 
             # Visit
-            callable(node)
-
+            # callable(node)
+            tree_list.append(node.value)
             # Go left
             if node.left is not None:
                 _walk(node.left)
@@ -81,10 +95,12 @@ class BinaryTree:
                 _walk(node.right)
 
         _walk(self.root)
+        return tree_list
 
     def post_order(self, callable=lambda node: print(node.value)):
         """go left, go right, visit
         """
+        tree_list = []
         def _walk(node=None):
             if node is None:
                 return
@@ -98,9 +114,10 @@ class BinaryTree:
                 _walk(node.right)
 
             # Visit
-            callable(node)
+            tree_list.append(node.value)
+            # callable(node)
 
         _walk(self.root)
+        return tree_list
 
-new_tree = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
-new_tree.in_order()
+
