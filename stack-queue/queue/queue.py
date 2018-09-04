40bc8a015c580352250c7baa80b2697f181ff848
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-09-03 17:14:26 -0700

    added docstrings, README, and error handling.

--- stack-queue/queue/queue.py
+++ stack-queue/queue/queue.py
@@ -3,12 +3,15 @@ from node import Node
 class Queue:
 
     def __init__(self, vals=None):
-       self.length = 0
-       self.front = None
-       self.back = None
-       if vals is not None:
-           for i in vals:
-               self.enqueue(i)
+        """This creates a new Queue, initializing it with a length of 0.
+        If an iterable is passed in, the queue will enqueue all of the corresponding values.
+        """
+        self.length = 0
+        self.front = None
+        self.back = None
+        if vals is not None:
+            for i in vals:
+                self.enqueue(i)
 
 
     def __len__(self):
@@ -18,6 +21,8 @@ class Queue:
         return f'Queue Front: {self.front.val} | Queue Back: {self.back.val} | Stack Length: {len(self)}'
 
     def enqueue(self, value):
+        """This method puts a value into the queue using FIFO methodology
+        """
         self.back = Node(value, self.back)
         self.length += 1
         if self.length == 1:
@@ -25,10 +30,15 @@ class Queue:
         return self.back.val
 
     def dequeue(self):
+        """This method takes a single value out of the queue using FIFO methodology
+        """
+        if self.length < 1:
+            raise ValueError
         current = self.back
         while current.next is not self.front:
             current = current.next
         temp = current.next
         current.next = None
         self.front = current
+        self.length -= 1
         return temp.val
